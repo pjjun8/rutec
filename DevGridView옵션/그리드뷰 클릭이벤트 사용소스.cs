c8624030a205be1c1dd7ad7be1226d6d1c97d515
pjjun8 <pjjun898@gmail.com>
2025-03-25 10:07:20 +0900

    Create 그리드뷰 클릭이벤트 사용소스.cs

---
+++ DevGridView옵션/그리드뷰 클릭이벤트 사용소스.cs
@@ -0,0 +1,35 @@
+/// <summary>
+        /// checkBox 사용으로 모두 주석처리
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void gridView1_RowCellClick(object sender, DevExpress.XtraGrid.Views.Grid.RowCellClickEventArgs e)
+        {
+            //// 현재 행 선택 여부 가져오기
+            //bool isSelected = gridView1.IsRowSelected(e.RowHandle);
+
+            //// 선택 상태 반전 (토글)
+            //if (isSelected)
+            //{
+            //    gridView1.UnselectRow(e.RowHandle); // 선택 해제
+            //}
+            //else
+            //{
+            //    gridView1.SelectRow(e.RowHandle); // 선택
+            //}
+
+
+            //if (e.RowHandle >= 0) // 유효한 행인지 확인
+            //{
+            //    primaryKey = gridView1.GetRowCellValue(e.RowHandle, "productCode").ToString();
+            //    MessageBox.Show($"선택 한 제품 Code: {primaryKey}");
+
+            //    nameText.Text = gridView1.GetRowCellValue(e.RowHandle, "productName").ToString(); ;
+            //    codeText.Text = gridView1.GetRowCellValue(e.RowHandle, "productCode").ToString(); ;
+            //    gtinText.Text = gridView1.GetRowCellValue(e.RowHandle, "productGTIN").ToString(); ;
+            //    sizeText.Text = gridView1.GetRowCellValue(e.RowHandle, "productSize").ToString(); ;
+            //    unitText.Text = gridView1.GetRowCellValue(e.RowHandle, "PUnit").ToString(); ;
+            //    iaNumberText.Text = gridView1.GetRowCellValue(e.RowHandle, "IANumber").ToString(); ;
+            //    sMethodText.Text = gridView1.GetRowCellValue(e.RowHandle, "SMethod").ToString(); ;
+            //}
+        }
