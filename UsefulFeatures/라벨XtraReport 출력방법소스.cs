4351cbb9246f81d46d466f67c76fedda7950ba75
pjjun8 <pjjun898@gmail.com>
2025-03-27 09:01:41 +0900

    Update 라벨XtraReport 출력방법소스.cs

--- UsefulFeatures/라벨XtraReport 출력방법소스.cs
+++ UsefulFeatures/라벨XtraReport 출력방법소스.cs
@@ -1,4 +1,4 @@
-/// <summary>
+        /// <summary>
         /// BindingList 를 DataTable 형식으로 변환
         /// </summary>
         /// <param name="list"></param>
@@ -37,3 +37,46 @@
             //rpt.DataSource = gridControl1.DataSource;
             //rpt.Print();
         }
+        private void simpleButton1_Click_1(object sender, EventArgs e)
+        {
+            PrintTableLabel();
+        }
+==================================================================================
+==================================================================================
+        /// <summary>
+        /// 라벨 프린트를 위한 함수
+        /// </summary>
+        private void PrintParametersLabel()
+        {
+            try
+            {
+                //xtraReport1.RequestParameters = false;
+                XtraReport1 xtraReport1 = new XtraReport1();
+                for (int i = 0; gridView1.RowCount - 1 > i; i++)
+                {
+
+                    xtraReport1.Parameters["LOT"].Value = gridView1.GetRowCellValue(i, "ProductLot").ToString();
+                    xtraReport1.Parameters["QTY"].Value = gridView1.GetRowCellValue(i, "QTY").ToString();
+                    xtraReport1.Parameters["ModelName"].Value = gridView1.GetRowCellValue(i, "ModelName").ToString();
+                    xtraReport1.Parameters["Holder"].Value = gridView1.GetRowCellValue(i, "Holder").ToString();
+                    xtraReport1.Parameters["Up_JIG"].Value = gridView1.GetRowCellValue(i, "Up_JIG").ToString();
+                    xtraReport1.Parameters["Down_JIG"].Value = gridView1.GetRowCellValue(i, "Down_JIG").ToString();
+                    xtraReport1.Parameters["Memo"].Value = gridView1.GetRowCellValue(i, "Memo").ToString();
+
+                    xtraReport1.CreateDocument();
+                    xtraReport1.Print();
+                    //xtraReport1.ShowPreviewDialog();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("정확한 입력값이 아닙니다.");
+                //MessageBox.Show(ex.ToString());
+            }
+        }
+        private void LabelOutPutButton_Click(object sender, EventArgs e)
+        {
+            PrintParametersLabel();
+        }
+
+
